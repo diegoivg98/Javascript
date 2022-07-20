8c6bc5905c17ce19711fc7f5d76e162b30e3a723
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-07-20 18:33:17 -0400

    Add files via upload

---
+++ ejercicio2/index.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Diego",
+    23,
+    true,
+    new Date(1998,07,27),
+    { 
+        titulo: "El Resplandor",
+        autor: "Stephen King",
+        fecha: new Date(1977, 0, 28),
+        url: "https://www.buscalibre.cl/libro-el-resplandor-stephen-king-debolsillo/9788490328729/p/35453017"
+      },
+];
+
+console.log(lista);
