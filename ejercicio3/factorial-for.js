85c5fe3356189ddcec4fc49e4550617e9713f7e3
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-07-21 01:13:08 -0400

    Delete ejercicio3 directory

--- ejercicio3/factorial-for.js
+++
@@ -1,5 +0,0 @@
-let factorial = 1;
-for (let i = 1; i <= 10 ; i++) {
-    factorial = factorial * i;
-    console.log(factorial);
-}
--- ejercicio3/factorial-while.js
+++
@@ -1,7 +0,0 @@
-let factorial = 1;
-let i = 1;
-while (i<=10) {
-    factorial = factorial * i;
-    i++;
-    console.log(factorial);
-}
