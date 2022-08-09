8bbd6624416aa17f945cab95ef240a7aa531f420
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-08-09 02:33:58 -0400

    Update index.js

--- EJERCICIOS/ejercicio21/index.js
+++ EJERCICIOS/ejercicio21/index.js
@@ -1,9 +1,5 @@
 /* Obtener el numero menor de un array, pero no negativo */
-const numeros = [1,-3,0];
-let min = numeros[0]; 
-for(let i = 0; i<numeros.length; i++) { 
-  if (min > numeros[i] && numeros[i] >= 0) {
-    min = numeros[i]; 
-  }
-}
-console.log('El menor es',min)
+const numbers = [1, -3, 0];
+const isPositive = (number) => number > 0
+const min = Math.min(...numbers.filter(isPositive))
+console.log(min)
